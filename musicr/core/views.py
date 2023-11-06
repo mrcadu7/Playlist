02a39324bce86488bea476e73b7c7423e6433f44
Cadú <139708523+mrcadu7@users.noreply.github.com>
2023-11-06 18:00:55 -0300

    os users só podem ver e alterar as proprias playlists quando logados

--- musicr/core/views.py
+++ musicr/core/views.py
@@ -4,7 +4,7 @@ from django import forms
 from core.models.playlist import Playlists, Addition
 from core.models.music import Song
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 
 
 class ArtistNameForm(forms.Form):
@@ -12,8 +12,14 @@ class ArtistNameForm(forms.Form):
 
 
 class AddToPlaylistForm(forms.Form):
-    playlist = forms.ModelChoiceField(queryset=Playlists.objects.all())
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+        super(AddToPlaylistForm, self).__init__(*args, **kwargs)
+        self.fields['playlist'].queryset = Playlists.objects.filter(user=user)
+
+    playlist = forms.ModelChoiceField(queryset=Playlists.objects.none())
     spotify_id = forms.CharField(widget=forms.HiddenInput())
+
 
 
 def autocomplete(request):
@@ -38,7 +44,8 @@ def index(request):
             if artist_data:
                 for album in artist_data['albums']:
                     for track in album['tracks']:
-                        track['form'] = AddToPlaylistForm(initial={'spotify_id': track['track_id']})
+                        if request.user.is_authenticated:
+                            track['form'] = AddToPlaylistForm(initial={'spotify_id': track['track_id']}, user=request.user)
             else:
                 message = f"Artista '{artist_name}' não encontrado no Spotify."
         
@@ -51,48 +58,81 @@ def index(request):
                                           'playlists': playlists})
 
 
+
+
 def add_to_playlist(request):
+    # Certifique-se de que o usuário está autenticado antes de tentar modificar a playlist
+    if not request.user.is_authenticated:
+        return JsonResponse({'success': False, 'message': 'Usuário não está logado'})
+
     if request.method == 'POST':
-        form = AddToPlaylistForm(request.POST)
+        form = AddToPlaylistForm(request.POST, user=request.user)
         if form.is_valid():
             playlist = form.cleaned_data['playlist']
             spotify_id = form.cleaned_data['spotify_id']
-
-            # Obtenha as informações da faixa
-            track_info = get_track_info(spotify_id)
-            
-            song, created = Song.objects.get_or_create(
-            spotify_id=spotify_id,
-            defaults={
-                'name': track_info['name'],
-                'duration': track_info['duration'],
-                'album': get_or_create_album(track_info['album_name'], track_info['artist_name']),
-            }
-        )
-            # Verifique se a música já está na playlist
-            if not Addition.objects.filter(song=song, playlist=playlist).exists():
-                # Crie um novo objeto Addition com a música e a playlist
-                addition = Addition.objects.create(song=song, playlist=playlist)
-                return JsonResponse({'success': True, 'message': 'Música adicionada com sucesso'})
+            # Obtenha o usuário logado
+            user = request.user
+            # Verifique se o usuário tem permissão para modificar a playlist
+            if playlist.user == user:
+                # Obtenha as informações da faixa
+                track_info = get_track_info(spotify_id)
+                
+                song, created = Song.objects.get_or_create(
+                spotify_id=spotify_id,
+                defaults={
+                    'name': track_info['name'],
+                    'duration': track_info['duration'],
+                    'album': get_or_create_album(track_info['album_name'], track_info['artist_name']),
+                }
+            )
+                # Verifique se a música já está na playlist
+                if not Addition.objects.filter(song=song, playlist=playlist).exists():
+                    # Crie um novo objeto Addition com a música e a playlist
+                    addition = Addition.objects.create(song=song, playlist=playlist)
+                    return JsonResponse({'success': True, 'message': 'Música adicionada com sucesso'})
+                else:
+                    return JsonResponse({'success': False, 'message': 'Música já existe na playlist'})
             else:
-                return JsonResponse({'success': False, 'message': 'Música já existe na playlist'})
+                return JsonResponse({'success': False, 'message': 'Usuário não tem permissão para modificar a playlist'})
 
     return JsonResponse({'message': 'Erro na solicitação AJAX'}, status=400)
 
 
+# def create_playlist(request):
+#     if request.method == 'POST':
+#         form = CreatePlaylistForm(request.POST) # necessário criar essa classe
+#         if form.is_valid():
+#             title = form.cleaned_data['title']
+#             description = form.cleaned_data['description']
+#             # Adicione o usuário à playlist
+#             user = request.user
+#             playlist = Playlists.objects.create(user=user, title=title, description=description)
+#             return JsonResponse({'success': True, 'message': 'Playlist criada com sucesso'})
+#     return JsonResponse({'message': 'Erro na solicitação AJAX'}, status=400)
+
+
 def view_playlist(request, playlist_id):
-    playlist = Playlists.objects.get(id=playlist_id)
-    additions = Addition.objects.filter(playlist=playlist)
-    songs = [addition.song for addition in additions]
-    playlists = Playlists.objects.all()
-    return render(request, 'view_playlist.html', {'songs': songs, 'playlist': playlist, 'playlists': playlists})
+    # Obtenha o usuário logado
+    user = request.user
+    # Filtre a playlist pelo usuário
+    playlist = Playlists.objects.filter(user=user, id=playlist_id).first()
+    if playlist:
+        additions = Addition.objects.filter(playlist=playlist)
+        songs = [addition.song for addition in additions]
+        playlists = Playlists.objects.all()
+        return render(request, 'view_playlist.html', {'songs': songs, 'playlist': playlist, 'playlists': playlists})
+    else:
+        return HttpResponse('Playlist não encontrada ou não pertence ao usuário', status=404)
 
 def list_playlists(request):
-    playlists = Playlists.objects.all()
+    # Obtenha o usuário logado
+    user = request.user
+    # Filtre as playlists pelo usuário
+    playlists = Playlists.objects.filter(user=user)
     playlist_songs = {}
     for playlist in playlists:
         additions = Addition.objects.filter(playlist=playlist)
         songs = [addition.song for addition in additions]
-        playlist_songs[playlist.title] = songs
-    return render(request, 'list_playlists.html', {'playlist_songs': playlist_songs})
+        playlist_songs[playlist] = songs
+    return render(request, 'list_playlists.html', {'playlist_songs': playlist_songs, 'user': user})
 
